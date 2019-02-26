eaa5a92186c19a0629adc5965621d37eec30ff5d
Angel <Angelbai320@gmail.com>
2019-02-26 19:33:37 +0800

    …

---
+++ blog/views.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+import markdown
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+
+
+# def index(request):
+#     return HttpResponse("欢迎")
+
+# def index(request):
+#     return render(request, 'blog/index.html', context={
+#                       'title': '我的博客首页',
+#                       'welcome': '欢迎访问我的博客首页'
+#                   })
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # 记得在顶部引入 markdown 模块
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog/detail.html', context={'post': post})
